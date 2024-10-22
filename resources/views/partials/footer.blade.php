831ad716769dde1d037c8410734d963c29389cdf
jacopolegovini <legovini96@gmail.com>
2024-10-22 15:30:09 +0200

    add draft header

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,15 @@
 <footer>
-    <h2>
-        Footer
-    </h2>
+    <div class="container social-footer">
+        <div class="sign-up">
+            <p>SIGN-UP NOW!</p>
+        </div>
+        <div class="social-icon">
+            <p>FOLLOW ME</p>
+            <img src="../../public/img/footer-facebook.png" alt="Facebook">
+            <img src="../../assets/img/img/footer-twitter.png" alt="Twitter">
+            <img src="../../assets/img/img/footer-youtube.png" alt="Youtube">
+            <img src="../../assets/img/img/footer-pinterest.png" alt="Pinterest">
+            <img src="../../assets/img/img/footer-periscope.png" alt="Periscope">
+        </div>
+    </div>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,14 @@
 <header>
-    <h1>
-        Header
-    </h1>
+    <div class="container header-flex">
+        <div class="img-header">
+            <img src="../../../public/img/dc-logo.png" alt="logo DC">
+        </div>
+        <div class="navbar-header">
+            <ul class="navbar-header-flex">
+                <li>
+                    <div>list</div>
+                </li>
+            </ul>
+        </div>
+    </div>
 </header>
