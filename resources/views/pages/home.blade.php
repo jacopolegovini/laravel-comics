331ad204c92adf9580030b289dc76dc3804cb076
jacopolegovini <legovini96@gmail.com>
2024-10-22 15:43:48 +0200

    db correctly implemented

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,7 +3,17 @@
 @section("page-title", "Homepage")
 
 @section("main-content")
+
+<?php $comics = config("db.comics") ?>
+
 <h1>
     Homepage
+    <ul>
+        {{-- @foreach ($comics as $comic )
+            <li>
+                {{$comic["title"]}}
+            </li>
+        @endforeach --}}
+    </ul>
 </h1>
 @endsection
